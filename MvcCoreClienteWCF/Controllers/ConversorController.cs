1c764fef880e755b7442d91b918b0363df46bc1a
anitapm03 <apereiramolinero@gmail.com>
2024-04-02 10:44:34 +0200

    a

---
+++ MvcCoreClienteWCF/Controllers/ConversorController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreClienteWCF.Services;
+
+namespace MvcCoreClienteWCF.Controllers
+{
+    public class ConversorController : Controller
+    {
+
+        public ServiceConversor service;
+
+        public ConversorController(ServiceConversor service)
+        {
+            this.service = service;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> 
+            Index(string numeros)
+        {
+            string data =
+                 await this.service.ConvertNumberToWordsAsync(numeros);
+            ViewData["DATOS"] = data;
+            return View();
+        }
+    }
+}
--- MvcCoreClienteWCF/Program.cs
+++ MvcCoreClienteWCF/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddTransient<ServiceCountries>();
+builder.Services.AddTransient<ServiceConversor>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
